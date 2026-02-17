bba8f2ac28f7fbd0589294466721b99de63459e0
cagnastylepay-png <cagnastyle.pay@gmail.com>
2026-02-17 22:39:43 +0100

    Remove max activation distance for prompt

--- TP.lua
+++ TP.lua
@@ -580,7 +580,6 @@ RenderedAnimals.ChildAdded:Connect(function(animal)
         local rarity = overHead:FindFirstChild("Rarity") and overHead.Rarity.Text or "Common"
 
         if buyConditionValidation(animalData.Price, name, income, rarity, mutation) then
-            prompt.MaxActivationDistance = 20
             prompt.PromptShown:Connect(function()
                 fireproximityprompt(prompt)
             end)
